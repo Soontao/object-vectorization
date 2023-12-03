16faa896a3ae8917016ff391d8135fecd5992933
Theo Sun <theo.sun.github@outlook.com>
2023-12-03 13:30:59 +0800

    chore: update

--- src/encoders/Encoder.ts
+++ src/encoders/Encoder.ts
@@ -1,9 +1,30 @@
 import { Vector } from "./type.js";
 
+/**
+ * Encoder
+ */
 export interface Encoder<T = any> {
+  /**
+   * encode value into vector
+   *
+   * @param value
+   */
   encode(value: T): Vector;
+  /**
+   * decode vector into plain value
+   *
+   * @param vec
+   */
   decode(vec: Vector): T;
+  /**
+   * get sub features list
+   *
+   * @param name current feature name
+   */
   features(name: string): Array<string>;
+  /**
+   * vector length
+   */
   get length(): number;
 }
 
