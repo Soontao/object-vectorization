1358304c4c0f065c912bd72705d5550ee6c2139d
Theo Sun <theo.sun.github@outlook.com>
2023-12-07 21:04:29 +0800

    chore: better test

--- jest.config.mjs
+++ jest.config.mjs
@@ -3,16 +3,7 @@
  */
 export default {
   preset: 'ts-jest/presets/default-esm',
-  extensionsToTreatAsEsm: [".ts"],
   testTimeout: 30 * 1000,
-  transform: {
-    ".(js|ts|tsx)": [
-      "ts-jest",
-      {
-        useESM: true,
-      },
-    ],
-  },
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
