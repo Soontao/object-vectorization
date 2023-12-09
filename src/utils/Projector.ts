2d94363eb81fd0b0367394e4c2665c5225bb8f6d
Theo Sun <theo.sun@outlook.com>
2023-12-09 17:18:48 +0800

    chore: update

---
+++ src/utils/Projector.ts
@@ -0,0 +1,42 @@
+import { ObjectEncoder, ObjectMetadata } from "../index.js";
+
+export function flattenObject(obj: any, parentKey?: string): Array<{ key: string; val: string }> {
+  const result: Array<{ key: string; val: string }> = [];
+
+  for (const [key, val] of Object.entries(obj)) {
+    const currentKey = parentKey ? `${parentKey}_${key}` : key;
+
+    if (typeof val === "object" && val !== null) {
+      // Recursively flatten nested objects
+      result.push(...flattenObject(val, currentKey));
+    } else {
+      if (["number", "string"].includes(typeof val)) {
+        result.push({ key: currentKey, val: String(val) });
+      } else {
+        result.push({ key: currentKey, val: JSON.stringify(val) });
+      }
+    }
+  }
+
+  return result;
+}
+
+export function createProjectorValues(meta: ObjectMetadata, items: Array<any>) {
+  const e = new ObjectEncoder(meta);
+  const vectors = items.map((i) => e.encode(i));
+  const tsv = vectors.map((v) => v.map(String).join("\t")).join("\n");
+  const metaHeaders = flattenObject(items[0]).map((v) => v.key);
+  const metadata = items.map((i) => flattenObject(i));
+  const metaTsv = [
+    metaHeaders.join("\t"),
+    ...metadata.map((meta) =>
+      Object.values(meta)
+        .map((v) => v.val)
+        .join("\t"),
+    ),
+  ].join("\n");
+  return {
+    tsv,
+    metaTsv,
+  };
+}
--- test/utils.test.js
+++ test/utils.test.js
@@ -1,5 +1,6 @@
 import { randomUUID } from "crypto";
 import { murmurhash3 } from "../src/utils/MurmurHash.js";
+import { createProjectorValues, flattenObject } from "../src/utils/Projector.js";
 
 describe("Utils Test Suite", () => {
   it("should return the correct hash value for a given key and seed", () => {
@@ -20,5 +21,63 @@ describe("Utils Test Suite", () => {
       expect(h).toBeGreaterThan(0);
       expect(h).toBeLessThan(2 ** 32 - 1);
     }
+  });
+
+  it('flattens a simple object', () => {
+    const input = { a: 1, b: { c: 2 } };
+    const expected = [
+      { key: 'a', val: '1' },
+      { key: 'b_c', val: '2' },
+    ];
+
+    const result = flattenObject(input);
+
+    expect(result).toEqual(expected);
+  });
+
+  it('flattens an object with arrays', () => {
+    const input = { a: { b: [{ c: 1 }, { c: 2 }] } };
+    const expected = [
+      { key: 'a_b_0_c', val: '1' },
+      { key: 'a_b_1_c', val: '2' },
+    ];
+
+    const result = flattenObject(input);
+
+    expect(result).toEqual(expected);
+  });
+
+  it('flattens an object with null values', () => {
+    const input = { a: null, b: { c: 2 } };
+    const expected = [
+      { key: 'a', val: 'null' },
+      { key: 'b_c', val: '2' },
+    ];
+
+    const result = flattenObject(input);
+
+    expect(result).toEqual(expected);
+  });
+
+  it('creates projector values', () => {
+    const meta = {
+      properties: [
+        {
+          name: 'property1',
+          type: 'category',
+          values: ['value1', 'value2', 'value3']
+        },
+      ],
+    };
+
+    const items = [
+      { property1: 'value1' },
+      { property1: 'value2' },
+    ];
+
+    const result = createProjectorValues(meta, items);
+
+    expect(result.tsv).toMatchSnapshot()
+    expect(result.metaTsv).toMatchSnapshot();
   });
 });
